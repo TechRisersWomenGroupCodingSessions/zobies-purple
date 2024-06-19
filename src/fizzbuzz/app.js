190aae5fd8f201c51010ca5d8fb808d26e870057
clarice_r <claricerdallorto@gmail.com>
2024-06-19 19:33:58 +0100

    Next step: refactor tests

--- src/fizzbuzz/app.js
+++ src/fizzbuzz/app.js
@@ -1,7 +1,14 @@
 function fizzbuzz(num) {
   let char = '';
-  for (let i=1; i < num; i++) {
-    char += i.toString() + '\n';
+  for (let i=1; i < num + 1; i++) {
+    if (i % 3 === 0) {
+      char += 'Fizz\n';
+    }
+    else{
+      char += i.toString() + '\n';
+    }
+    
+    console.log(char);
   }
   return char.trim();
   }
--- src/fizzbuzz/app.test.js
+++ src/fizzbuzz/app.test.js
@@ -1,9 +1,14 @@
 const fizzbuzz = require('./app');
 
 test('prints out the number 1', () => {
-  expect(fizzbuzz(1)).toBe(1);
+  expect(fizzbuzz(1)).toBe("1");
 });
 
+//Todo: Refactor or delete
 test('prints out the number 1 till 3', () => {
   expect(fizzbuzz(3)).toBe('1\n2\n3');
+});
+
+test('prints out Fizz when number is divisible by 3', () => {
+  expect(fizzbuzz(3)).toBe('1\n2\nFizz');
 });
