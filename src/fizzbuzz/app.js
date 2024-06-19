ca5f3e00f57cadd5d4abaf928e8ab753c48cdf9a
GurTati <tatiana.gurova.j@gmail.com>
2024-06-19 19:20:49 +0100

    test_2

--- src/fizzbuzz/app.js
+++ src/fizzbuzz/app.js
@@ -1,5 +1,9 @@
-function sum(a, b) {
-    return a + b;
+function fizzbuzz(num) {
+  let char = '';
+  for (let i=1; i < num; i++) {
+    char += i.toString() + '\n';
+  }
+  return char.trim();
   }
 
-module.exports = sum;
+module.exports = fizzbuzz;
--- src/fizzbuzz/app.test.js
+++ src/fizzbuzz/app.test.js
@@ -1,5 +1,9 @@
-const sum = require('./app');
+const fizzbuzz = require('./app');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('prints out the number 1', () => {
+  expect(fizzbuzz(1)).toBe(1);
+});
+
+test('prints out the number 1 till 3', () => {
+  expect(fizzbuzz(3)).toBe('1\n2\n3');
 });
