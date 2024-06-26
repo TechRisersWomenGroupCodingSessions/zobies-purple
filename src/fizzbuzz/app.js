f016f8f79f539c54fe2b21f4528a413a04c220e0
GurTati <tatiana.gurova.j@gmail.com>
2024-06-26 18:30:26 +0100

    add logic to the app.js

--- src/fizzbuzz/app.js
+++ src/fizzbuzz/app.js
@@ -1,11 +1,13 @@
 function fizzbuzz(num) {
   let char = '';
   for (let i=1; i < num + 1; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      char += 'FizzBuzz\n';
+    } else if (i % 3 === 0) {
       char += 'Fizz\n';
-    }
-    else if (i % 5 === 0) {
-      char += 'Buzz\n'
+      
+    } else if (i % 5 === 0) {
+      char += 'Buzz\n';
     }
     else{
       char += i.toString() + '\n';
