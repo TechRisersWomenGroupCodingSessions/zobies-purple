674191c3bfba07a27e22360d30abde2e5101db48
Magdalena Piatkowska <magda.piatk@gmail.com>
2024-06-26 18:20:21 +0100

    Test for printing out FizzBuzz

--- src/fizzbuzz/app.test.js
+++ src/fizzbuzz/app.test.js
@@ -11,3 +11,7 @@ test('prints out Fizz when number is divisible by 3', () => {
 test('prints out Buzz when number is divisible by 5', () => {
   expect(fizzbuzz(5)).toBe('1\n2\nFizz\n4\nBuzz');
 });
+
+test('prints out FizzBuzz when number is divisible by 15', () => {
+  expect(fizzbuzz(15)).toContain('\n14\nFizzBuzz');
+});
